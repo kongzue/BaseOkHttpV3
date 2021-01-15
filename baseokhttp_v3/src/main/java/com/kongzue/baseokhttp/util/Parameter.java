570d8af628036628cec672213236522cc3dee2af
kongzue <myzcxhh@live.cn>
2021-01-16 07:26:28 +0800

    3.2.1.1 - enhance Parameter function.

--- baseokhttp_v3/src/main/java/com/kongzue/baseokhttp/util/Parameter.java
+++ baseokhttp_v3/src/main/java/com/kongzue/baseokhttp/util/Parameter.java
@@ -93,4 +93,106 @@ public class Parameter extends TreeMap<String, Object> {
             return null;
         }
     }
+    
+    public String getString(String key) {
+        return getString(key, "");
+    }
+    
+    public String getString(String key, String defaultValue) {
+        Object value = get(key);
+        if (isNull(String.valueOf(value))) {
+            return defaultValue;
+        }
+        return value == null ? "" : String.valueOf(value);
+    }
+    
+    public int getInt(String key) {
+        return getInt(key, 0);
+    }
+    
+    public int getInt(String key, int emptyValue) {
+        int result = emptyValue;
+        try {
+            result = Integer.parseInt(get(key) + "");
+        } catch (Exception e) {
+        }
+        return result;
+    }
+    
+    public boolean getBoolean(String key) {
+        return getBoolean(key, false);
+    }
+    
+    public boolean getBoolean(String key, boolean emptyValue) {
+        boolean result = emptyValue;
+        try {
+            result = Boolean.parseBoolean(get(key) + "");
+        } catch (Exception e) {
+        }
+        return result;
+    }
+    
+    public long getLong(String key) {
+        return getLong(key, 0);
+    }
+    
+    public long getLong(String key, long emptyValue) {
+        long result = emptyValue;
+        try {
+            result = Long.parseLong(get(key) + "");
+        } catch (Exception e) {
+        }
+        return result;
+    }
+    
+    public short getShort(String key) {
+        return getShort(key, (short) 0);
+    }
+    
+    public short getShort(String key, short emptyValue) {
+        short result = emptyValue;
+        try {
+            result = Short.parseShort(get(key) + "");
+        } catch (Exception e) {
+        }
+        return result;
+    }
+    
+    public double getDouble(String key) {
+        return getDouble(key, 0);
+    }
+    
+    public double getDouble(String key, double emptyValue) {
+        double result = emptyValue;
+        try {
+            result = Double.parseDouble(get(key) + "");
+        } catch (Exception e) {
+        }
+        return result;
+    }
+    
+    public float getFloat(String key) {
+        return getFloat(key, 0);
+    }
+    
+    public float getFloat(String key, float emptyValue) {
+        float result = emptyValue;
+        try {
+            result = Float.parseFloat(get(key) + "");
+        } catch (Exception e) {
+        }
+        return emptyValue;
+    }
+    
+    private boolean isNull(String s) {
+        if (s == null || s.trim().isEmpty() || "null".equals(s)) {
+            return true;
+        }
+        return false;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        return toString().equals(o.toString());
+    }
 }
