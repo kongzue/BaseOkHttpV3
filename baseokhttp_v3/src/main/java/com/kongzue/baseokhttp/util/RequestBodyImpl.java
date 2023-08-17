d11593b12c591a0ea50d3c3a3e87d378a83ecd85
Kongzue <myzcxhh@live.cn>
2023-08-17 19:50:43 +0800

    3.2.5

--- baseokhttp_v3/src/main/java/com/kongzue/baseokhttp/util/RequestBodyImpl.java
+++ baseokhttp_v3/src/main/java/com/kongzue/baseokhttp/util/RequestBodyImpl.java
@@ -10,40 +10,46 @@ import okio.Okio;
 import okio.Sink;
 
 public abstract class RequestBodyImpl extends RequestBody {
-    
+
     private final RequestBody requestBody;
     private BufferedSink bufferedSink;
-    
+
     public RequestBodyImpl(RequestBody requestBody) {
         this.requestBody = requestBody;
     }
-    
+
     @Override
     public MediaType contentType() {
         return requestBody.contentType();
     }
-    
+
     @Override
     public long contentLength() throws IOException {
         return requestBody.contentLength();
     }
-    
+
     @Override
     public void writeTo(BufferedSink sink) throws IOException {
         if (bufferedSink == null) {
-            bufferedSink = Okio.buffer(sink(sink));
+            try {
+                bufferedSink = Okio.buffer(sink(sink));
+            } catch (Exception e) {
+                if (BaseOkHttp.DEBUGMODE) {
+                    e.printStackTrace();
+                }
+            }
         }
         requestBody.writeTo(bufferedSink);
         //必须调用flush，否则最后一部分数据可能不会被写入
         bufferedSink.flush();
     }
-    
+
     private Sink sink(Sink sink) {
         return new ForwardingSink(sink) {
             private long current;
             private long total;
             private long last = 0;
-            
+
             @Override
             public void write(okio.Buffer source, long byteCount) throws IOException {
                 super.write(source, byteCount);
@@ -59,6 +65,6 @@ public abstract class RequestBodyImpl extends RequestBody {
             }
         };
     }
-    
+
     public abstract void loading(long current, long total, boolean done);
 }
